6f7e559fac3b500653521abf4465c51e5c4bb15d
Ranjeet singh <singhrj414@gmail.com>
2023-06-29 20:04:49 +0530

    new

---
+++ Task/PHP/practice/print_r.php
@@ -0,0 +1,11 @@
+<?php
+
+$arr = array("Maths"=>100,"Eng"=>80,"Gujarati"=>"85","Hindi"=>80,"Pass"=>true);
+print_r($arr);
+echo "<pre>";
+print_r($arr);
+var_dump($arr);
+var_export($arr);
+// echo "</pre>";
+var_dump($arr);
+?>
