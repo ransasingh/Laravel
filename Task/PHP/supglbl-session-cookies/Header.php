6d6dff9bb69f324cfbe5b6307baf33d6210c26fe
Ranjeet singh <singhrj414@gmail.com>
2023-07-08 14:05:17 +0530

    new

--- Task/PHP/supglbl-session-cookies/Header.php
+++ Task/PHP/supglbl-session-cookies/Header.php
@@ -41,6 +41,7 @@
           <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Account</a>
           <div class="dropdown-menu">
             <a class="dropdown-item" href="login.php">login</a>
+            <a class="dropdown-item" href="logout.php">logout</a>
             <a class="dropdown-item" href="#">Another action</a>
             <a class="dropdown-item" href="#">Something else here</a>
             <div class="dropdown-divider"></div>
