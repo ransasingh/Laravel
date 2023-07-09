1c610e18a45d2998c35eb6952eb134dad9642a2b
Ranjeet singh <singhrj414@gmail.com>
2023-07-09 20:19:12 +0530

    new

--- Assignment/php/1 marksheet.php
+++ Assignment/php/1 marksheet.php
@@ -53,6 +53,34 @@
         echo "Percentage: $percentage% <br>";
         echo "Grade: $grade";
     }
+    echo "<br>";
+    $day = "Thursday";
+
+switch ($day) {
+    case "Monday":
+        echo "Monday is not Thursday.";
+        break;
+    case "Tuesday":
+        echo "Tuesday is not Thursday.";
+        break;
+    case "Wednesday":
+        echo "Wednesday is not Thursday.";
+        break;
+    case "Thursday":
+        echo "Thursday is found!";
+        break;
+    case "Friday":
+        echo "Friday is not Thursday.";
+        break;
+    case "Saturday":
+        echo "Saturday is not Thursday.";
+        break;
+    case "Sunday":
+        echo "Sunday is not Thursday.";
+        break;
+    default:
+        echo "Invalid day.";
+}
     ?>
 </body>
 </html>
