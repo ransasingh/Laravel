452e6a4c4218a89c91885c872c0ba71476cfd0e7
Ranjeet singh <singhrj414@gmail.com>
2023-09-21 13:51:47 +0530

    new

--- Task/PHP/practice/Assesment/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-session_destroy();
-setcookie("username","",time()-1*60*60);
-setcookie("password","",time()-1*60*60);
-setcookie("email","",time()-1*60*60);
-header("location:login.php");
-?>
