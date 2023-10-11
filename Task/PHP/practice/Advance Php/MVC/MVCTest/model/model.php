498733c0bf23841d2da5a53d557aed764d9df44a
Ranjeet singh <singhrj414@gmail.com>
2023-10-11 19:50:23 +0530

    new

--- Task/PHP/practice/Advance Php/MVC/MVCTest/model/model.php
+++ Task/PHP/practice/Advance Php/MVC/MVCTest/model/model.php
@@ -1,9 +1,10 @@
 <?php
-class model{
-    public $conncetion= "";
-   public function __construct()
+class model
+{
+    public $conncetion = "";
+    public function __construct()
     {
-        $this->conncetion = new mysqli("localhost", "root","","masterdatabase");
+        $this->conncetion = new mysqli("localhost", "root", "", "masterdatabase");
         // if ( $this->conncetion ) {
 
         //     echo "success";
@@ -13,11 +14,13 @@ class model{
         // }
 
     }
+
+    //select query using fetch a data from database
     function select($tbl, $where = "")
     {
         $SQL = "Select * from $tbl ";
         // echo $SQL
-    
+
 
         if ($where != "") {
             $SQL .= " WHERE ";
@@ -47,14 +50,85 @@ class model{
         }
         return $Res;
     }
-    function insert(){
-        
 
+    //insert data /form data insert  into table by insert query 
+    function insert($tbl, $data)
+    {
+        // echo "<pre>";
+        // print_r($data);
+        // $clm= array_keys( $data);
+        // print_r($clm);
+        $clm = implode(",", array_keys($data));
+        $vals = implode("','", $data);
+
+        $SQL = "INSERT INTO $tbl ($clm) VALUES ('$vals')";
+        // echo $SQL;
+        $SQLEx = $this->connection->query($SQL);
+        // echo "<pre>";
+        if ($SQLEx > 0) {
+            $Res['Code'] = 1;
+            $Res['Data'] = 1;
+            $Res['Msg'] = "Success";
+        } else {
+            $Res['Code'] = 0;
+            $Res['Data'] = 0;
+            $Res['Msg'] = "Try again";
+        }
+        return $Res;
     }
-    function update(){
 
+    //edit a data and after a updata a value in database with particular data 
+    function update($tbl, $data, $where)
+    {
+        $SQL = "update $tbl set  ";
+        foreach ($data as $key => $value) {
+            $SQL .= "$key = '$value' ,";
+        }
+        // echo $SQL;
+        $SQL = rtrim($SQL, ",");
+        $SQL .= " WHERE";
+        foreach ($where as $key => $value) {
+            $SQL .= " $key = '$value ' AND";
+        }
+        $SQL = rtrim($SQL, "AND");
+        // echo $SQL;
+        // exit;
+        $sqlex = $this->connection->query($SQL);
+        // print_r($sqlex);
+        // exit;
+        if ($sqlex > 0) {
+            $ResponceData['Data'] = "1";
+            $ResponceData['Msg'] = "Success";
+            $ResponceData['Code'] = "1";
+        } else {
+            $ResponceData['Data'] = "0";
+            $ResponceData['Msg'] = "Try Again";
+            $ResponceData['Code'] = "0";
+        }
+        return $ResponceData;
     }
-    function delete(){
 
+  //delete a particular data 
+    function delete($tbl, $where)
+    {
+        $SQL = " DELETE FROM $tbl WHERE";
+        foreach ($where as $key => $value) {
+            $SQL .= " $key = '$value' AND";
+        }
+        $SQL = rtrim($SQL, "AND");
+        // echo $SQL;
+        $SQLEx = $this->connection->query($SQL);
+        // print_r($SQLEx);
+        if ($SQLEx > 0) {
+            $ResponceData['Data'] = "1";
+            $ResponceData['Msg'] = "Success";
+
+            $ResponceData['Code'] = "1";
+        } else {
+            $ResponceData['Data'] = "0";
+            $ResponceData['Msg'] = "Try Agian";
+            $ResponceData['Code'] = "0";
+        }
+        return $ResponceData;
     }
 }
